86e6f311d51168e8e176265706d0bdd33977e513
vadimt <vadimt@google.com>
2020-10-22 16:25:35 -0700

    Add CB listener for LatencyTracker metrics
    Test will be added separately, which will be
    tracked by the same bug if the reviewers don't mind.
    
    Test: atest PlatformScenarioTests:android.platform.test.scenario.sysui.lockscreen.LockScreenWithPasswordMicrobenchmark -- --test-arg com.android.tradefed.testtype.AndroidJUnitTest:device-listeners:android.device.collectors.UiActionLatencyListener --module-arg PlatformScenarioTests:instrumentation-arg:iterations:=3 --module-arg PlatformScenarioTests:instrumentation-arg:drop-cache:=false --module-arg PlatformScenarioTests:instrumentation-arg:kill-app:=false
    
    Bug: 171604120
    Change-Id: I22a46500c2a6b2fad087be5abf3946c726d2046d

--- libraries/collectors-helper/statsd/src/com/android/helpers/UiActionLatencyHelper.java
+++ libraries/collectors-helper/statsd/src/com/android/helpers/UiActionLatencyHelper.java
@@ -31,53 +31,42 @@ import java.util.Map;
  * Helper consisting of helper methods to set system interactions configs in statsd and retrieve the
  * necessary information from statsd using the config id.
  */
-public class UiInteractionFrameInfoHelper implements ICollectorHelper<StringBuilder> {
+public class UiActionLatencyHelper implements ICollectorHelper<StringBuilder> {
 
-    private static final String LOG_TAG = UiInteractionFrameInfoHelper.class.getSimpleName();
+    private static final String LOG_TAG = UiActionLatencyHelper.class.getSimpleName();
 
     private final StatsdHelper mStatsdHelper = new StatsdHelper();
 
-    /** Set up the system interactions jank statsd config. */
+    /** Set up the system actions latency statsd config. */
     @Override
     public boolean startCollecting() {
-        Log.i(LOG_TAG, "Adding system interactions config to statsd.");
+        Log.i(LOG_TAG, "Adding system actions latency config to statsd.");
         List<Integer> atomIdList = new ArrayList<>();
-        atomIdList.add(Atom.UI_INTERACTION_FRAME_INFO_REPORTED_FIELD_NUMBER);
+        atomIdList.add(Atom.UI_ACTION_LATENCY_REPORTED_FIELD_NUMBER);
         return mStatsdHelper.addEventConfig(atomIdList);
     }
 
-    /** Collect the system interactions jank metrics from the statsd. */
+    /** Collect the system actions latency metrics from the statsd. */
     @Override
     public Map<String, StringBuilder> getMetrics() {
         Log.i(LOG_TAG, "get metrics.");
-        Map<String, StringBuilder> frameInfoMap = new HashMap<>();
+        Map<String, StringBuilder> latenciesMap = new HashMap<>();
         for (EventMetricData dataItem : mStatsdHelper.getEventMetrics()) {
             final Atom atom = dataItem.getAtom();
-            if (atom.hasUiInteractionFrameInfoReported()) {
-                final AtomsProto.UIInteractionFrameInfoReported uiInteractionFrameInfoReported =
-                        atom.getUiInteractionFrameInfoReported();
+            if (atom.hasUiActionLatencyReported()) {
+                final AtomsProto.UIActionLatencyReported uiActionLatencyReported =
+                        atom.getUiActionLatencyReported();
 
-                final String interactionType =
-                        uiInteractionFrameInfoReported.getInteractionType().toString();
-
-                MetricUtility.addMetric(
-                        MetricUtility.constructKey("cuj", interactionType, "total_frames"),
-                        uiInteractionFrameInfoReported.getTotalFrames(),
-                        frameInfoMap);
+                final String action = String.valueOf(uiActionLatencyReported.getAction());
 
                 MetricUtility.addMetric(
-                        MetricUtility.constructKey("cuj", interactionType, "missed_frames"),
-                        uiInteractionFrameInfoReported.getMissedFrames(),
-                        frameInfoMap);
-
-                MetricUtility.addMetric(
-                        MetricUtility.constructKey("cuj", interactionType, "max_frame_time_nanos"),
-                        uiInteractionFrameInfoReported.getMaxFrameTimeNanos(),
-                        frameInfoMap);
+                        MetricUtility.constructKey("latency", action),
+                        uiActionLatencyReported.getLatencyMillis(),
+                        latenciesMap);
             }
         }
 
-        return frameInfoMap;
+        return latenciesMap;
     }
 
     /** Remove the statsd config. */
