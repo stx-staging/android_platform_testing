afdb8b03264cffc5f488bbb6ecff3a61bbe97506
vadimt <vadimt@google.com>
2020-11-03 14:38:30 -0800

    Switching frame duration metric to milliseconds
    The typical value is several million, and is hard to read
    on dashboards.
    
    Test: presubmit
    Bug: 170441505
    Change-Id: I27e139d72b53849ae56fc619f67e9f0d244c2c22

--- libraries/collectors-helper/utilities/src/com/android/helpers/MetricUtility.java
+++ libraries/collectors-helper/utilities/src/com/android/helpers/MetricUtility.java
@@ -44,6 +44,23 @@ public class MetricUtility {
     }
 
     /**
+     * Add metric to the result map. If metric key already exist append the new metric.
+     *
+     * @param metricKey Unique key to track the metric.
+     * @param metric metric to track.
+     * @param resultMap map of all the metrics.
+     */
+    public static void addMetric(
+            String metricKey, double metric, Map<String, StringBuilder> resultMap) {
+        resultMap.compute(
+                metricKey,
+                (key, value) ->
+                        (value == null)
+                                ? new StringBuilder().append(metric)
+                                : value.append(METRIC_SEPARATOR).append(metric));
+    }
+
+    /**
      * Add metric to the result map. If metric key already exist increment the value by 1.
      *
      * @param metricKey Unique key to track the metric.
