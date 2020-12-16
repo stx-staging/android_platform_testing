c506a01a4147ce65d3a56e7fdfc88775315865c5
Nataniel Borges <natanieljr@google.com>
2020-12-16 15:38:27 +0100

    Introduce Flicker- Subject, SubjectException and FailureStrategy
    Currently the flicker subjects (Layers, WM and EventLog). This base class doesn't add any information related to the subject being tested (when using failWithoutActual) or the toString of the object (when using failWithActual).
    
    Moreover, these methods are protected and cannot be accessed form the test specification. When a user throws a test exception the trace information is then missing.
    
    With this FlickerSubject class we can ensure that the failures will always contain debugging information
    
    Bug: 162923992
    Test: atest FlickerLibTest
    Change-Id: I33bb42284dff091f8063b51a45e4fe4d31f0238b

---
+++ libraries/flicker/src/com/android/server/wm/flicker/traces/FlickerTraceSubject.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.wm.flicker.traces
+
+import com.android.server.wm.flicker.assertions.Assertion
+import com.android.server.wm.flicker.assertions.AssertionsChecker
+import com.android.server.wm.flicker.assertions.FlickerSubject
+import com.google.common.truth.FailureMetadata
+
+/**
+ * Base subject for flicker trace assertions
+ */
+abstract class FlickerTraceSubject<EntrySubject : FlickerSubject>(
+    fm: FailureMetadata,
+    data: Any?
+) : FlickerSubject(fm, data) {
+    protected val assertionsChecker = AssertionsChecker<EntrySubject>()
+    private var newAssertionBlock = true
+
+    abstract val subjects: List<EntrySubject>
+
+    protected fun addAssertion(name: String, assertion: Assertion<EntrySubject>) {
+        if (newAssertionBlock) {
+            assertionsChecker.add(name, assertion)
+        } else {
+            assertionsChecker.append(name, assertion)
+        }
+        newAssertionBlock = false
+    }
+
+    /**
+     * Run the assertions for all trace entries
+     */
+    fun forAllEntries() {
+        assertionsChecker.test(subjects)
+    }
+
+    /**
+     * User-defined entry point for the first trace entry
+     */
+    fun first(): EntrySubject = subjects.first()
+
+    /**
+     * User-defined entry point for the last trace entry
+     */
+    fun last(): EntrySubject = subjects.last()
+
+    /**
+     * Signal that the last assertion set is complete. The next assertion added will start a new
+     * set of assertions.
+     *
+     * E.g.: checkA().then().checkB()
+     *
+     * Will produce two sets of assertions (checkA) and (checkB) and checkB will only be checked
+     * after checkA passes.
+     */
+    protected fun startAssertionBlock() {
+        newAssertionBlock = true
+    }
+
+    /**
+     * Checks whether all the trace entries on the list are visible for more than one consecutive
+     * entry
+     *
+     * @param [visibleEntries] a list of all the entries with their name and index
+     */
+    protected fun visibleEntriesShownMoreThanOneConsecutiveTime(
+        visibleEntries: List<Pair<String, Int>>
+    ) {
+        visibleEntries.distinct()
+            .groupBy({ it.first }, { it.second })
+            .forEach { (layerName, indexesList) ->
+                val listOfConsecutiveNumbers = getListsOfConsecutiveNumbers(indexesList)
+                listOfConsecutiveNumbers.forEach {
+                    if (it.size < 2) {
+                        val entry = subjects[it.first()]
+                        entry.fail("Element $layerName is not visible for 2 entries " +
+                            "(${it.joinToString(", ")})")
+                    }
+                }
+            }
+    }
+
+    private fun getListsOfConsecutiveNumbers(indexList: List<Int>):
+        MutableList<MutableList<Int>> {
+        val sequences = mutableListOf(mutableListOf(indexList.first()))
+        indexList.drop(1).forEach {
+            val currBlock = sequences.last()
+            if (it != (currBlock.last() + 1)) {
+                sequences.add(mutableListOf())
+            }
+            sequences.last().add(it)
+        }
+        return sequences
+    }
+}
--- libraries/flicker/test/src/com/android/server/wm/flicker/Utils.kt
+++ libraries/flicker/test/src/com/android/server/wm/flicker/Utils.kt
@@ -18,11 +18,14 @@ package com.android.server.wm.flicker
 
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
+import com.android.server.wm.flicker.traces.FlickerSubjectException
 import com.android.server.wm.traces.common.layers.LayersTrace
 import com.android.server.wm.traces.common.windowmanager.WindowManagerTrace
 import com.android.server.wm.traces.parser.layers.LayersTraceParser
 import com.android.server.wm.traces.parser.windowmanager.WindowManagerTraceParser
 import com.google.common.io.ByteStreams
+import com.google.common.truth.ExpectFailure
+import com.google.common.truth.TruthFailureSubject
 
 internal fun readWmTraceFromFile(relativePath: String): WindowManagerTrace {
     return try {
@@ -48,4 +51,37 @@ internal fun readTestFile(relativePath: String): ByteArray {
     val context: Context = InstrumentationRegistry.getInstrumentation().context
     val inputStream = context.resources.assets.open("testdata/$relativePath")
     return ByteStreams.toByteArray(inputStream)
+}
+
+/**
+ * Runs `r` and asserts that an exception of type `expectedThrowable` is thrown.
+ * @param expectedThrowable the type of throwable that is expected to be thrown
+ * @param r the [Runnable] which is run and expected to throw.
+ * @throws AssertionError if `r` does not throw, or throws a runnable that is not an
+ * instance of `expectedThrowable`.
+ */
+// TODO: remove once Android migrates to JUnit 4.13, which provides assertThrows
+fun assertThrows(expectedThrowable: Class<out Throwable>, r: () -> Any): Throwable {
+    try {
+        r()
+    } catch (t: Throwable) {
+        when {
+            expectedThrowable.isInstance(t) -> return t
+            t is Exception ->
+                throw AssertionError("Expected $expectedThrowable, but got ${t.javaClass}", t)
+            // Re-throw Errors and other non-Exception throwables.
+            else -> throw t
+        }
+    }
+    error("Expected $expectedThrowable, but nothing was thrown")
+}
+
+fun assertFailure(failure: Throwable?): TruthFailureSubject {
+    val target = when (failure) {
+        is FlickerSubjectException -> failure.cause
+        is AssertionError -> failure
+        else -> error("Expected assertion error, received $failure")
+    }
+    require(target is AssertionError) { "Unknown failure $target" }
+    return ExpectFailure.assertThat(target)
 }
