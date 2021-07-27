46ac430745e4028fc56915ee8d1dc6744420d334
Ioana Moraru <ioanamoraru@google.com>
2021-07-27 19:09:27 +0000

    Add interfaces for the Flicker as a Service
    Define the main interfaces uses for the Flicker as a Service project
    
    Bug: 194685676
    Test: See the new added files from service package
    Change-Id: Ibf541ab2cacb0d1c4a64a463eb29bdb4789f5668

---
+++ libraries/flicker/src/com/android/server/wm/flicker/rules/WMFlickerServiceRule.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.wm.flicker.rules
+
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * Collect the WM and SF traces and call the WM Flicker Service after the test
+ */
+class WMFlickerServiceRule : TestWatcher() {
+    override fun finished(description: Description?) {
+        // TODO(b/195121324): Collect the traces and invoke the service
+    }
+}
