fe348d3c8ca4c65419d51a1771d5ceccd4a099df
Gopinath Elanchezhian <gelanchezhian@google.com>
2021-09-27 13:33:38 -0700

    Backport multi user config.
    Bug: b/190233841
    
    Test: Builds successfully.
    Change-Id: I950903cf794ff052b5c2454f6a15d72f42ffcb3b
    Merged-In: I65e9f22ac6aea25d87a45458e4c43e3b8412d9dc

--- scripts/perfetto-setup/Android.mk
+++ scripts/perfetto-setup/Android.mk
@@ -55,6 +55,16 @@ LOCAL_PREBUILT_MODULE_FILE := prebuilts/tools/linux-x86_64/perfetto/configs/trac
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := trace_config_multi_user.textproto
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS := notice
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/local/tmp
+LOCAL_PREBUILT_MODULE_FILE := frameworks/base/apct-tests/perftests/multiuser/trace_configs/trace_config_multi_user.textproto
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := perfetto_trace_processor_shell
 LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
 LOCAL_LICENSE_CONDITIONS := notice
