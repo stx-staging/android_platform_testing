4178158edcff2fc9042ff6bbaa59105b4c02f6f0
Vova Sharaienko <sharaienko@google.com>
2021-12-21 02:28:33 +0000

    Added Helper & Listener to collect StatsdStatsReport
    This report represents statsd metadata
    - added config stats parsing and corresponding tests
    
    Bug: 204890512
    Test: atest CollectorsHelperTest:com.android.helpers.StatsdStatsHelperTest
    Change-Id: Id5b6ef7590db0848f55ab63a774919d47214508f

--- libraries/collectors-helper/statsd/src/com/android/helpers/StatsdStatsHelper.java
+++ libraries/collectors-helper/statsd/src/com/android/helpers/StatsdStatsHelper.java
@@ -31,6 +31,11 @@ public class StatsdStatsHelper implements ICollectorHelper<Long> {
 
     static final String STATSDSTATS_PREFIX = "statsdstats";
     static final String ATOM_STATS_PREFIX = "atom_stats";
+    static final String MATCHER_STATS_PREFIX = "matcher_stats";
+    static final String CONDITION_STATS_PREFIX = "condition_stats";
+    static final String METRIC_STATS_PREFIX = "metric_stats";
+    static final String ALERT_STATS_PREFIX = "alert_stats";
+    static final String CONFIG_STATS_PREFIX = "config_stats";
 
     interface IStatsdHelper {
         StatsLog.StatsdStatsReport getStatsdStatsReport();
@@ -73,11 +78,12 @@ public class StatsdStatsHelper implements ICollectorHelper<Long> {
 
         final StatsLog.StatsdStatsReport report = mStatsdHelper.getStatsdStatsReport();
         populateAtomStats(report.atomStats, resultMap);
+        populateConfigStats(report.configStats, resultMap);
 
         return resultMap;
     }
 
-    private void populateAtomStats(
+    private static void populateAtomStats(
             StatsLog.StatsdStatsReport.AtomStats[] atomStats, Map<String, Long> resultMap) {
         final String metricKeyPrefix =
                 MetricUtility.constructKey(STATSDSTATS_PREFIX, ATOM_STATS_PREFIX);
@@ -91,6 +97,95 @@ public class StatsdStatsHelper implements ICollectorHelper<Long> {
             resultMap.put(
                     MetricUtility.constructKey(metricKeyPrefixWithTag, "error_count"),
                     Long.valueOf(dataItem.errorCount));
+        }
+    }
+
+    private static void populateConfigStats(
+            StatsLog.StatsdStatsReport.ConfigStats[] configStats, Map<String, Long> resultMap) {
+        final String metricKeyPrefix =
+                MetricUtility.constructKey(STATSDSTATS_PREFIX, CONFIG_STATS_PREFIX);
+
+        for (final StatsLog.StatsdStatsReport.ConfigStats dataItem : configStats) {
+            final String metricKeyPrefixWithTag =
+                    MetricUtility.constructKey(metricKeyPrefix, String.valueOf(dataItem.id));
+
+            resultMap.put(
+                    MetricUtility.constructKey(metricKeyPrefixWithTag, "metric_count"),
+                    Long.valueOf(dataItem.metricCount));
+            resultMap.put(
+                    MetricUtility.constructKey(metricKeyPrefixWithTag, "condition_count"),
+                    Long.valueOf(dataItem.conditionCount));
+            resultMap.put(
+                    MetricUtility.constructKey(metricKeyPrefixWithTag, "matcher_count"),
+                    Long.valueOf(dataItem.matcherCount));
+            resultMap.put(
+                    MetricUtility.constructKey(metricKeyPrefixWithTag, "alert_count"),
+                    Long.valueOf(dataItem.alertCount));
+
+            populateMatcherStats(dataItem.matcherStats, resultMap, metricKeyPrefixWithTag);
+            populateConditionStats(dataItem.conditionStats, resultMap, metricKeyPrefixWithTag);
+            populateMetricStats(dataItem.metricStats, resultMap, metricKeyPrefixWithTag);
+            populateAlertStats(dataItem.alertStats, resultMap, metricKeyPrefixWithTag);
+        }
+    }
+
+    private static void populateMetricStats(
+            StatsLog.StatsdStatsReport.MetricStats[] stats,
+            Map<String, Long> resultMap,
+            String metricKeyPrefix) {
+        for (final StatsLog.StatsdStatsReport.MetricStats dataItem : stats) {
+            final String metricKey =
+                    MetricUtility.constructKey(
+                            metricKeyPrefix,
+                            METRIC_STATS_PREFIX,
+                            String.valueOf(dataItem.id),
+                            "max_tuple_counts");
+            resultMap.put(metricKey, Long.valueOf(dataItem.maxTupleCounts));
+        }
+    }
+
+    private static void populateConditionStats(
+            StatsLog.StatsdStatsReport.ConditionStats[] stats,
+            Map<String, Long> resultMap,
+            String metricKeyPrefix) {
+        for (final StatsLog.StatsdStatsReport.ConditionStats dataItem : stats) {
+            final String metricKey =
+                    MetricUtility.constructKey(
+                            metricKeyPrefix,
+                            CONDITION_STATS_PREFIX,
+                            String.valueOf(dataItem.id),
+                            "max_tuple_counts");
+            resultMap.put(metricKey, Long.valueOf(dataItem.maxTupleCounts));
+        }
+    }
+
+    private static void populateMatcherStats(
+            StatsLog.StatsdStatsReport.MatcherStats[] stats,
+            Map<String, Long> resultMap,
+            String metricKeyPrefix) {
+        for (final StatsLog.StatsdStatsReport.MatcherStats dataItem : stats) {
+            final String metricKey =
+                    MetricUtility.constructKey(
+                            metricKeyPrefix,
+                            MATCHER_STATS_PREFIX,
+                            String.valueOf(dataItem.id),
+                            "matched_times");
+            resultMap.put(metricKey, Long.valueOf(dataItem.matchedTimes));
+        }
+    }
+
+    private static void populateAlertStats(
+            StatsLog.StatsdStatsReport.AlertStats[] stats,
+            Map<String, Long> resultMap,
+            String metricKeyPrefix) {
+        for (final StatsLog.StatsdStatsReport.AlertStats dataItem : stats) {
+            final String metricKey =
+                    MetricUtility.constructKey(
+                            metricKeyPrefix,
+                            ALERT_STATS_PREFIX,
+                            String.valueOf(dataItem.id),
+                            "alerted_times");
+            resultMap.put(metricKey, Long.valueOf(dataItem.alertedTimes));
         }
     }
 
