f95d5ed8e8df442a2262705c31eb44bef9906221
Vadim Tryshev <vadimt@google.com>
2022-01-06 21:23:31 +0000

    Revert "Checking for correct metric in UiInteractionFrameInfoHelperTest"
    This reverts commit 1d5472a16a75d5474eb273073ea8dcc170938d23.
    
    Reason for revert: Causing build break
    
    Change-Id: Ieb63f4f1ff26edce0593970b122376281d38e5a6

--- libraries/collectors-helper/statsd/test/src/com/android/helpers/UiInteractionFrameInfoHelperTest.java
+++ libraries/collectors-helper/statsd/test/src/com/android/helpers/UiInteractionFrameInfoHelperTest.java
@@ -21,8 +21,7 @@ import android.app.Application;
 import android.os.SystemClock;
 import android.platform.helpers.HelperAccessor;
 import android.platform.helpers.INotificationHelper;
-import android.platform.systemui_tapl.NotificationShade;
-import android.platform.systemui_tapl.Root;
+import android.support.test.uiautomator.UiObject2;
 
 import androidx.test.runner.AndroidJUnit4;
 
@@ -78,22 +77,24 @@ public class UiInteractionFrameInfoHelperTest {
         notificationHelper.get().setAppName(Application.getProcessName());
 
         // The CUJ
-        final NotificationShade shade = Root.get().openNotificationShadeViaSwipe();
-        shade.postBigTextNotification(null /* pkg */);
-        NotificationShade.cancelNotifications();
-        shade.close();
+        notificationHelper.get().open();
+        UiObject2 notification = notificationHelper.get().postBigTextNotification(null /* pkg */);
+        notificationHelper.get().showGuts(notification);
+        notificationHelper.get().hideGuts(notification);
+        notificationHelper.get().cancelNotifications();
+        notificationHelper.get().exit();
 
         // Checking metrics produced by the CUJ.
         final Map<String, StringBuilder> frameMetrics = mInteractionFrameHelper.getMetrics();
         assertTrue(
-                "No metric cuj_NOTIFICATION_ADD_missed_frames",
-                frameMetrics.containsKey("cuj_NOTIFICATION_ADD_missed_frames"));
+                "No metric missed_frames_cuj_SHADE_SCROLL_FLING missing",
+                frameMetrics.containsKey("cuj_SHADE_SCROLL_FLING_missed_frames"));
         assertTrue(
-                "No metric cuj_NOTIFICATION_ADD_total_frames",
-                frameMetrics.containsKey("cuj_NOTIFICATION_ADD_total_frames"));
+                "No metric total_frames_cuj_SHADE_SCROLL_FLING",
+                frameMetrics.containsKey("cuj_SHADE_SCROLL_FLING_total_frames"));
         assertTrue(
-                "No metric cuj_NOTIFICATION_ADD_max_frame_time_ms",
-                frameMetrics.containsKey("cuj_NOTIFICATION_ADD_max_frame_time_ms"));
+                "No metric max_frame_time_nanos_cuj_SHADE_SCROLL_FLING",
+                frameMetrics.containsKey("cuj_SHADE_SCROLL_FLING_max_frame_time_ms"));
 
         assertTrue(mInteractionFrameHelper.stopCollecting());
         HelperTestUtility.sendKeyCode(KEYCODE_HOME);
