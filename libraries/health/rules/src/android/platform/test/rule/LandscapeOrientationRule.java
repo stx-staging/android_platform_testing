db030d6fa9b0099b4f834a8f88ae0a4b366ad57d
vadimt <vadimt@google.com>
2022-02-10 17:19:56 -0800

    Quick fix for an AOSP build breakage
    Apparently systemui-helper doesn't exist in aosp.
    
    For now, copied all methods from there.
    
    Bug: 202567877
    Test: local build
    Change-Id: I68a029b23b5048c06202811cb94b3900a6367527

--- libraries/health/rules/src/android/platform/test/rule/LandscapeOrientationRule.java
+++ libraries/health/rules/src/android/platform/test/rule/LandscapeOrientationRule.java
@@ -17,14 +17,16 @@ package android.platform.test.rule;
 
 import android.graphics.Rect;
 import android.os.RemoteException;
-import android.platform.helpers.CommonUtils;
 
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiObject2;
 
 import com.android.launcher3.tapl.LauncherInstrumentation;
 
+import org.junit.Assert;
 import org.junit.runner.Description;
+
+import java.util.function.Supplier;
 
 /**
  * Locks landscape orientation before running a test and goes back to natural orientation
@@ -32,6 +34,47 @@ import org.junit.runner.Description;
  */
 public class LandscapeOrientationRule extends TestWatcher {
     private final LauncherInstrumentation mLauncher = new LauncherInstrumentation();
+    private static final int WAIT_TIME_MS = 10000;
+    private static final int SLEEP_MS = 100;
+
+    private interface Condition {
+        boolean isTrue() throws Throwable;
+    }
+
+    private static void waitForNullDiag(Supplier<String> diagnostics) {
+        final String[] lastDiag = new String[1];
+        waitForCondition(() -> lastDiag[0], () -> (lastDiag[0] = diagnostics.get()) == null);
+    }
+
+    private static void waitForCondition(Supplier<String> message, Condition condition) {
+        waitForCondition(message, condition, WAIT_TIME_MS);
+    }
+
+    private static void waitForCondition(
+            Supplier<String> message, Condition condition, long timeoutMs) {
+        final long startTime = android.os.SystemClock.uptimeMillis();
+        while (android.os.SystemClock.uptimeMillis() < startTime + timeoutMs) {
+            try {
+                if (condition.isTrue()) {
+                    return;
+                }
+            } catch (Throwable t) {
+                throw new RuntimeException(t);
+            }
+            android.os.SystemClock.sleep(SLEEP_MS);
+        }
+
+        // Check once more before failing.
+        try {
+            if (condition.isTrue()) {
+                return;
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+
+        Assert.fail(message.get());
+    }
 
     @Override
     protected void starting(Description description) {
@@ -40,7 +83,7 @@ public class LandscapeOrientationRule extends TestWatcher {
             mLauncher.setEnableRotation(true);
             getUiDevice().setOrientationLeft();
 
-            CommonUtils.waitForNullDiag(
+            waitForNullDiag(
                     () -> {
                         final UiObject2 launcher =
                                 getUiDevice()
