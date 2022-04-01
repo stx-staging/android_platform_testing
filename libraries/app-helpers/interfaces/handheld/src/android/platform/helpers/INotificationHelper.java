4b059321894af22034d1aca471a6e666bc644d1f
Nicolo' Mazzucato <nicomazz@google.com>
2022-04-01 12:01:43 +0000

    Add basic integration tests for shelf
    This adds tests for the behaviour of the notification shelf when there is an overflow of notifications.
    
    Test: atest ShelfInvisibleWithLowNotifications && atest ShelfVisibleAtTheBottom
    Bug: 227733494
    Change-Id: I3041d58fd70155069265a589eefd4a051fe6ac11

--- libraries/app-helpers/interfaces/handheld/src/android/platform/helpers/INotificationHelper.java
+++ libraries/app-helpers/interfaces/handheld/src/android/platform/helpers/INotificationHelper.java
@@ -136,6 +136,15 @@ public interface INotificationHelper extends IAppHelper {
     }
 
     /**
+     * @return the notification shelf, containing overflow notifications that don't fit in the
+     *     screen. If unavailable, returns null.
+     */
+    @Nullable
+    default UiObject2 getNotificationShelf() {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    /**
      * Setup Expectations: Shade is open
      *
      * <p>Posts multiple bubble notification. These notifications are associated with a conversation
