b6e80261c86501c0a6cc3b8a0ff4030f51c06b4e
Kean Mariotti <keanmariotti@google.com>
2022-05-23 08:32:02 +0000

    Add flagsVerbose property to Layer.kt
    The flagVerbose  property was originally added by winscope and got lost during the refactoring/deduplication
    of the common code (flicker/winscope) that decodes the SF protobuf traces.
    
    Fixes: 222165551
    Test: atest FlickerLibTest:com.android.server.wm.flicker.layers.LayerTest
    
    Change-Id: Ibf62c2db6ae9bd623c90ad08ef56dd2d856c9e0d

---
+++ libraries/flicker/test/src/com/android/server/wm/flicker/layers/LayerTest.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.wm.flicker.layers
+
+import com.android.server.wm.traces.common.ActiveBuffer
+import com.android.server.wm.traces.common.Color
+import com.android.server.wm.traces.common.Rect
+import com.android.server.wm.traces.common.RectF
+import com.android.server.wm.traces.common.layers.Layer
+import com.android.server.wm.traces.common.layers.Transform
+import com.google.common.truth.Truth.assertThat
+import org.junit.FixMethodOrder
+import org.junit.Test
+import org.junit.runners.MethodSorters
+
+/**
+ * Contains [Layer] tests. To run this test: `atest FlickerLibTest:LayerTest`
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class LayerTest {
+    @Test
+    fun hasVerboseFlagsProperty() {
+        assertThat(makeLayerWithFlags(0x0).verboseFlags)
+            .isEqualTo("")
+
+        assertThat(makeLayerWithFlags(0x1).verboseFlags)
+            .isEqualTo("HIDDEN (0x1)")
+
+        assertThat(makeLayerWithFlags(0x2).verboseFlags)
+            .isEqualTo("OPAQUE (0x2)")
+
+        assertThat(makeLayerWithFlags(0x40).verboseFlags)
+            .isEqualTo("SKIP_SCREENSHOT (0x40)")
+
+        assertThat(makeLayerWithFlags(0x80).verboseFlags)
+            .isEqualTo("SECURE (0x80)")
+
+        assertThat(makeLayerWithFlags(0x100).verboseFlags)
+            .isEqualTo("ENABLE_BACKPRESSURE (0x100)")
+
+        assertThat(makeLayerWithFlags(0x200).verboseFlags)
+            .isEqualTo("DISPLAY_DECORATION (0x200)")
+
+        assertThat(makeLayerWithFlags(0x400).verboseFlags)
+            .isEqualTo("IGNORE_DESTINATION_FRAME (0x400)")
+
+        assertThat(makeLayerWithFlags(0xc3).verboseFlags)
+            .isEqualTo("HIDDEN|OPAQUE|SKIP_SCREENSHOT|SECURE (0xc3)")
+    }
+
+    private fun makeLayerWithFlags(flags: Int): Layer {
+        return Layer("", 0, 0, 0, null, ActiveBuffer.EMPTY, flags,
+            RectF.EMPTY, Color.EMPTY, false, -1f, -1f,
+            "", null, Transform.EMPTY, RectF.EMPTY, -1,
+            -1, Transform.EMPTY, -1, RectF.EMPTY, Rect.EMPTY,
+            -1, null, false, -1, -1,
+            Transform.EMPTY, Color.EMPTY, RectF.EMPTY, Transform.EMPTY, null)
+    }
+}
