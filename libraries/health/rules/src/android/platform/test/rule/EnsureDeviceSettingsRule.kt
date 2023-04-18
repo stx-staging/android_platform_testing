9299db67e3aa2161ce1aa7f67f5db6bc2a25ea9b
Uwais Ashraf <uwaisashraf@google.com>
2023-04-18 15:05:26 +0000

    Adds a rule to ensure the additional device setting requirement of being in adb root mode.
    Bug: b/278713694
    Test: Ran the rule using OpenCloseNotificationShade
    Change-Id: I8d5d07e9b9df556b76c6708e9ff02593e723a619

--- libraries/health/rules/src/android/platform/test/rule/EnsureDeviceSettingsRule.kt
+++ libraries/health/rules/src/android/platform/test/rule/EnsureDeviceSettingsRule.kt
@@ -1,20 +1,29 @@
 package android.platform.test.rule
 
 import android.os.SystemProperties
+import android.platform.uiautomator_helpers.DeviceHelpers.shell
 import android.provider.Settings
 import org.junit.runner.Description
 
-/**
- * Making sure "Stay awake" setting from Developer settings is set so the screen doesn't turn off
- * while tests are running and that "Test Harness" mode is on.
- *
- * Setting value of "Stay awake" is bit-based with 4 bits responsible for different types of
- * charging. So the value is device-dependent but non-zero value means the settings is on.
- * See [Settings.Global.STAY_ON_WHILE_PLUGGED_IN] for more information.
- */
+/** Making sure settings that are required to run tests are present and in the correct states. */
 class EnsureDeviceSettingsRule : TestWatcher() {
 
     override fun starting(description: Description?) {
+        assertAdbRootEnabled()
+        assertTestHarnessEnabled()
+        assertStayAwakeEnabled()
+    }
+
+    private fun assertAdbRootEnabled() {
+        val adbIdResult = uiDevice.shell("id -u").trim()
+
+        if (adbIdResult != "0") {
+            throw AssertionError("ADB root access is required but disabled. " +
+                    "Restart ADB as root using `adb root`.")
+        }
+    }
+
+    private fun assertTestHarnessEnabled() {
         val mobileHarnessModeEnabled = SystemProperties.getBoolean(TEST_HARNESS_PROP, false)
         if (!mobileHarnessModeEnabled) {
             throw AssertionError("'Test harness' mode is required but disabled. " +
@@ -22,12 +31,19 @@ class EnsureDeviceSettingsRule : TestWatcher() {
                     "It is also recommended to restart Nexus Launcher after doing this using " +
                     "`adb shell am force-stop $LAUNCHER_PACKAGE`")
         }
+    }
 
+    /**
+     * Setting value of "Stay awake" is bit-based with 4 bits responsible for different types of
+     * charging. So the value is device-dependent but non-zero value means the settings is on.
+     * See [Settings.Global.STAY_ON_WHILE_PLUGGED_IN] for more information.
+     */
+    private fun assertStayAwakeEnabled() {
         val stayAwakeResult =
-            Settings.Global.getInt(
-                context.contentResolver,
-                Settings.Global.STAY_ON_WHILE_PLUGGED_IN
-            )
+                Settings.Global.getInt(
+                        context.contentResolver,
+                        Settings.Global.STAY_ON_WHILE_PLUGGED_IN
+                )
         if (stayAwakeResult == 0) {
             throw AssertionError("'Stay awake' option in developer settings should be enabled")
         }
@@ -36,7 +52,5 @@ class EnsureDeviceSettingsRule : TestWatcher() {
     private companion object {
         const val TEST_HARNESS_PROP = "ro.test_harness"
         const val LAUNCHER_PACKAGE = "com.google.android.apps.nexuslauncher"
-
-        const val TAG = "EnsureDeviceSettingsRule"
     }
 }
