29ff2d8483609a208f454660bc57efbb34e58773
Ram Peri <ramperi@google.com>
2023-08-10 17:30:32 -0400

    Invoke Espress.idle to ensure that all UI events are processed.
    Test: atest -c SystemUIGoogleRoboRNGTests
    Bug: 294753198
    Change-Id: I0fbdd6065bf89b4ece3002b19a08b83da9c00a8f

--- libraries/screenshot/src/main/java/platform/test/screenshot/WindowCapture.kt
+++ libraries/screenshot/src/main/java/platform/test/screenshot/WindowCapture.kt
@@ -7,6 +7,7 @@ import android.os.Looper
 import android.view.PixelCopy
 import android.view.Window
 import androidx.concurrent.futures.ResolvableFuture
+import androidx.test.espresso.Espresso
 
 /*
  * This file was forked from androidx/test/core/view/WindowCapture.kt.
@@ -27,6 +28,13 @@ fun Window.generateBitmapFromPixelCopy(
                 )
             }
         }
+
+    // We need to flush all the events in the UI queue before taking a screenshot.
+    // This can be guaranteed by waiting for an onSuccess callback, as implemented by toBitmap, but
+    // in robolectric mode,
+    //  we have to use this sync call as the procssing is done on the main looper and therefore we
+    // need a sync based solution.
+    Espresso.onIdle()
     PixelCopy.request(
         this,
         boundsInWindow,
