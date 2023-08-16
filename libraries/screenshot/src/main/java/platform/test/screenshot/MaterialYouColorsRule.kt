94fe8b53b35c464ca781415c056184cec6622e1c
Jordan Demeulenaere <jdemeulenaere@google.com>
2023-08-16 18:21:58 +0200

    Make MaterialYouColorsRule work with new Material colors (1/2)
    The previous implementation of MaterialYouColorsRule was not working
    with the new material you tokens. This CL change the implementation of
    this rule to override the setting used to compute the colors, like we do
    in MaterialYouPaletteTest.
    
    Given that this CL will require updating all existing golden images
    using MaterialYou colors, I changed the fixed colors from the previous
    ugly orange to the nice green/blue presets available in the wallpaper
    picker.
    
    Bug: 290209259
    Test: atest MaterialYouColorsRuleTest
    Test: atest SystemUIGoogleScreenshotTests
    Change-Id: I06db11a041bac5daaa9281b4ce4838c638901d53

--- libraries/screenshot/src/main/java/platform/test/screenshot/MaterialYouColorsRule.kt
+++ libraries/screenshot/src/main/java/platform/test/screenshot/MaterialYouColorsRule.kt
@@ -17,148 +17,111 @@
 package platform.test.screenshot
 
 import android.content.Context
-import android.util.SparseIntArray
-import android.widget.RemoteViews
-import androidx.annotation.VisibleForTesting
+import android.os.SystemClock
+import android.provider.Settings
 import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 
 /**
- * A rule to overload the target context system colors by [colors].
+ * A rule to change the system colors using the given [seedColorHex].
  *
- * This is especially useful to apply the colors before you start an activity using an
- * [ActivityScenarioRule] or any other rule, given that the colors must be [applied]
- * [MaterialYouColors.apply] *before* doing any resource resolution.
+ * This is especially useful to change the colors before starting an activity using an
+ * [ActivityScenarioRule] or any other rule.
  */
-class MaterialYouColorsRule(private val colors: MaterialYouColors = MaterialYouColors.Orange) :
-    TestRule {
+class MaterialYouColorsRule(
+    private val colorsConfiguration: String = DEFAULT_SCREENSHOT_CONFIGURATION
+) : TestRule {
+    constructor(
+        seedColorHex: String,
+        accentColorHex: String
+    ) : this(
+        """
+        {
+            "android.theme.customization.system_palette": "$seedColorHex",
+            "android.theme.customization.accent_color": "$accentColorHex"
+        }
+    """
+            .trimIndent()
+    )
+
     override fun apply(base: Statement, description: Description): Statement {
         return object : Statement() {
             override fun evaluate() {
-                colors.apply(InstrumentationRegistry.getInstrumentation().targetContext)
-                base.evaluate()
+                val context = InstrumentationRegistry.getInstrumentation().targetContext
+                val previousSettingValue =
+                    Settings.Secure.getString(
+                        context.contentResolver,
+                        Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES
+                    )
+
+                if (previousSettingValue == colorsConfiguration) {
+                    // The system colors should already be what we expect, so no need to set them.
+                    base.evaluate()
+                    return
+                }
+
+                try {
+                    changeColorSetting(context, colorsConfiguration)
+                    base.evaluate()
+                } finally {
+                    // Restore the previous colors.
+                    changeColorSetting(context, previousSettingValue)
+                }
             }
         }
     }
-}
 
-/**
- * A util class to overload the Material You colors of a [Context] to some fixed values. This can be
- * used by screenshot tests so that device-specific colors don't impact the outcome of the test.
- *
- * @see apply
- */
-class MaterialYouColors(
-    @get:VisibleForTesting val colors: SparseIntArray,
-) {
-    /**
-     * Apply these colors to [context].
-     *
-     * Important: No resource resolution must have be done on the context given to that method.
-     */
-    fun apply(context: Context) {
-        RemoteViews.ColorResources.create(context, colors)?.apply(context)
+    private fun changeColorSetting(context: Context, settingValue: String?) {
+        // Save the current value of the system_primary_dark color.
+        val primaryDarkBefore = context.getColor(android.R.color.system_primary_dark)
+
+        // Change the Material You colors configuration.
+        Settings.Secure.putString(
+            context.contentResolver,
+            Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+            settingValue
+        )
+
+        // Wait for the setting propagation by waiting for the system_primary_dark color to change.
+        waitFor(
+            timeoutMs = 5_000,
+            errorMessage = "system_primary_dark color did not change within 5s"
+        ) {
+            primaryDarkBefore != context.getColor(android.R.color.system_primary_dark)
+        }
+    }
+
+    private fun waitFor(
+        timeoutMs: Long,
+        errorMessage: String,
+        stepMs: Long = 50,
+        condition: () -> Boolean,
+    ) {
+        val start = SystemClock.uptimeMillis()
+        while (!condition()) {
+            if (SystemClock.uptimeMillis() - start > timeoutMs) {
+                Assert.fail(errorMessage)
+            }
+            Thread.sleep(stepMs)
+        }
     }
 
     companion object {
-        private const val FIRST_RESOURCE_COLOR_ID = android.R.color.system_neutral1_0
-        private const val LAST_RESOURCE_COLOR_ID = android.R.color.system_accent3_1000
-
         /**
-         * An instance of [MaterialYouColors] with orange seed, that can be used directly by tests.
+         * A nice green/blue configuration that is one of the suggest presets in the wallpaper
+         * picker.
          */
-        val Orange = fromColors(ORANGE)
-
-        /**
-         * Create a [MaterialYouColors] from [colors], where:
-         * - `colors[i]` should be the value of `FIRST_RESOURCE_COLOR_ID + i`.
-         * - [colors] must contain all values of all system colors, i.e. `colors.size` should be
-         *   `LAST_RESOURCE_COLOR_ID - FIRST_RESOURCE_COLOR_ID + 1`.
-         */
-        private fun fromColors(colors: IntArray): MaterialYouColors {
-            val expectedSize = LAST_RESOURCE_COLOR_ID - FIRST_RESOURCE_COLOR_ID + 1
-            check(colors.size == expectedSize) {
-                "colors should have exactly $expectedSize elements"
-            }
-
-            val sparseArray = SparseIntArray(/* initialCapacity= */ expectedSize)
-            colors.forEachIndexed { i, color ->
-                sparseArray.put(FIRST_RESOURCE_COLOR_ID + i, color)
-            }
-
-            return MaterialYouColors(sparseArray)
-        }
+        val DEFAULT_SCREENSHOT_CONFIGURATION =
+            """
+                {
+                    "android.theme.customization.system_palette": "B1EBFF",
+                    "android.theme.customization.accent_color": "B1EBFF",
+                    "android.theme.customization.theme_style":"FRUIT_SALAD"
+                }
+            """
+                .trimIndent()
     }
 }
-
-/** Some orange colors, extracted using 0xA66800 as seed color. */
-private val ORANGE =
-    intArrayOf(
-        -1,
-        -1025,
-        -397337,
-        -1318439,
-        -3226179,
-        -5068125,
-        -6844535,
-        -8620690,
-        -10199721,
-        -11778496,
-        -13291734,
-        -14738666,
-        -16777216,
-        -1,
-        -1025,
-        -4386,
-        -925488,
-        -2833227,
-        -4675174,
-        -6451583,
-        -8293273,
-        -9937840,
-        -11516615,
-        -13095132,
-        -14542319,
-        -16777216,
-        -1,
-        -1025,
-        -4386,
-        -8775,
-        -476303,
-        -2449319,
-        -4422078,
-        -6394838,
-        -8235756,
-        -10076672,
-        -12113408,
-        -13953280,
-        -16777216,
-        -1,
-        -1025,
-        -4386,
-        -139843,
-        -2113118,
-        -4020599,
-        -5862288,
-        -7703977,
-        -9348543,
-        -11058389,
-        -12636905,
-        -14149627,
-        -16777216,
-        -1,
-        -393241,
-        -1640507,
-        -2561608,
-        -4403810,
-        -6180476,
-        -7956885,
-        -9667501,
-        -11246787,
-        -12760281,
-        -14207725,
-        -15524094,
-        -16777216,
-    )
