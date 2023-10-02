15838f7b8f6b6baa8e10291adbcc1bbbde35110d
Nicolo' Mazzucato <nicomazz@google.com>
2023-10-02 22:49:02 +0000

    Add specs for external displays
    Bug: 299256162
    Test: KeyguardPresentationScreenshotTest
    Change-Id: I1e83fe358e00fb39e0f331b7f99b65421c8f02bf

--- libraries/screenshot/src/main/java/platform/test/screenshot/DefaultDeviceEmulationSpec.kt
+++ libraries/screenshot/src/main/java/platform/test/screenshot/DefaultDeviceEmulationSpec.kt
@@ -28,6 +28,19 @@ private val PhoneAndTabletMinimalSpec =
     DeviceEmulationSpec.forDisplays(Displays.Phone, isDarkTheme = false) +
         DeviceEmulationSpec.forDisplays(Displays.Tablet, isDarkTheme = true, isLandscape = false)
 
+val DeviceEmulationSpec.Companion.externalDisplaysMinimal
+    get() = externalDisplaysMinimalSpec
+
+private val externalDisplaysMinimalSpec: List<DeviceEmulationSpec> =
+    DeviceEmulationSpec.forDisplays(
+        Displays.External480p,
+        Displays.External720p,
+        Displays.External1080p,
+        Displays.External4k,
+        isDarkTheme = false,
+        isLandscape = true
+    )
+
 object Displays {
     val Phone =
         DisplaySpec(
@@ -42,6 +55,38 @@ object Displays {
             "tablet",
             width = 2560,
             height = 1600,
+            densityDpi = 320,
+        )
+
+    val External480p =
+        DisplaySpec(
+            "external480p",
+            width = 720,
+            height = 480,
+            densityDpi = 142,
+        )
+
+    val External720p =
+        DisplaySpec(
+            "external720p",
+            width = 1280,
+            height = 720,
+            densityDpi = 213,
+        )
+
+    val External1080p =
+        DisplaySpec(
+            "external1080p",
+            width = 1920,
+            height = 1080,
+            densityDpi = 320,
+        )
+
+    val External4k =
+        DisplaySpec(
+            "external4k",
+            width = 3840,
+            height = 2160,
             densityDpi = 320,
         )
 }
